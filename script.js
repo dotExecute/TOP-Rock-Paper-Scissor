9bc8ced87cb4de9a965a72c457519964797cfe4e
Swapney Raul <raulswapney97@gmail.com>
2025-08-23 18:18:23 +0530

    Add functionality to get the human input and store it

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// generate random num between 0-3
 function getComputerChoice(){
     let randNum = Math.floor(Math.random() * 3);
 
+    // assign num 0-3 to rock paper sissor respectively
     if(randNum === 0){
         return "rock";
     }else if(randNum === 1){
@@ -10,3 +12,10 @@ function getComputerChoice(){
     }
 }
 console.log(getComputerChoice());
+
+// get human choice and store it
+function getHumanChoice(){
+    let humanChoice = prompt("What's your Choice?");
+    return humanChoice;
+}
+console.log(getHumanChoice());
