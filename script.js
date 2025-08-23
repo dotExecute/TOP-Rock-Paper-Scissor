f9845be7c5d81a8c47958940f84ac9b8dfe79554
Swapney Raul <raulswapney97@gmail.com>
2025-08-23 17:31:22 +0530

    Add functionality to get computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+function getComputerChoice(){
+    let randNum = Math.floor(Math.random() * 3);
+
+    if(randNum === 0){
+        return "rock";
+    }else if(randNum === 1){
+        return "paper";
+    }else {
+        return "scissor";
+    }
+}
+console.log(getComputerChoice());
