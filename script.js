f0199698aab0f8c1e84c24e22eb0e591b2db2539
Swapney Raul <raulswapney97@gmail.com>
2025-08-25 20:27:32 +0530

    Add game win final score logic and console message

--- script.js
+++ script.js
@@ -2,74 +2,82 @@ let humanScore = 0;
 let computerScore = 0;
 
 // generate random num between 0-3
-function getComputerChoice(){
-    let randNum = Math.floor(Math.random() * 3);
+function getComputerChoice() {
+  let randNum = Math.floor(Math.random() * 3);
 
-    // assign num 0-3 to rock paper sissor respectively
-    if(randNum === 0){
-        return "rock";
-    }else if(randNum === 1){
-        return "paper";
-    }else {
-        return "scissor";
-    }
+  // assign num 0-3 to rock paper sissor respectively
+  if (randNum === 0) {
+    return "rock";
+  } else if (randNum === 1) {
+    return "paper";
+  } else {
+    return "scissor";
+  }
 }
 
 // get human choice and store it
-function getHumanChoice(){
-    let humanChoice = prompt("What's your Choice?");
+function getHumanChoice() {
+  let humanChoice = prompt("What's your Choice?");
 
-    // returning the choice
-    return humanChoice;
+  // returning the choice
+  return humanChoice;
 }
 
 // logic for a single round, just a bunch of if else statements
-function playRound(human, computer){
-    if(computer === "rock" && human === "scissor"){
-        console.log("Computer Wins!");
-        computerScore++;
-    }else if (computer === "scissor" && human == "paper"){
-        console.log("Computer Wins!");
-        computerScore++;
-    }else if (computer === "paper" && human == "rock"){
-        console.log("Computer Wins!");
-        computerScore++;
-    }else if(human === "rock" && computer === "scissor"){
-        console.log("Human Wins!");
-        humanScore++;
-    }else if (human === "scissor" && computer == "paper"){
-        console.log("Human Wins!");
-        humanScore++;
-    }else if (human === "paper" && computer == "rock"){
-        console.log("Human Wins!");
-        humanScore++;
-    }else if (computer === human){
-        console.log("Tie!");
-    }else{
-        console.log("Undefined");
-    }
+function playRound(human, computer) {
+  if (computer === "rock" && human === "scissor") {
+    console.log("Computer Wins!");
+    computerScore++;
+  } else if (computer === "scissor" && human == "paper") {
+    console.log("Computer Wins!");
+    computerScore++;
+  } else if (computer === "paper" && human == "rock") {
+    console.log("Computer Wins!");
+    computerScore++;
+  } else if (human === "rock" && computer === "scissor") {
+    console.log("Human Wins!");
+    humanScore++;
+  } else if (human === "scissor" && computer == "paper") {
+    console.log("Human Wins!");
+    humanScore++;
+  } else if (human === "paper" && computer == "rock") {
+    console.log("Human Wins!");
+    humanScore++;
+  } else if (computer === human) {
+    console.log("Tie!");
+  } else {
+    console.log("Undefined");
+  }
 }
 
 // add a function named playGame
-function playGame(){
-    // make a for loop to count up for i<5
-    for (i = 0; i < 3; i++){
-        // add the funtions inside the loop
-        const newCompSelection = getComputerChoice();
-        const newHumanSelection = getHumanChoice();
-        playRound(newHumanSelection, newCompSelection);
+function playGame() {
+  // make a for loop to count up for i<5
+  for (i = 0; i < 3; i++) {
+    // add the funtions inside the loop
+    const newCompSelection = getComputerChoice();
+    const newHumanSelection = getHumanChoice();
+    playRound(newHumanSelection, newCompSelection);
 
-        // loggig the human and computer selection in the console
-        console.log(newCompSelection);
-        console.log(newHumanSelection); 
-    }
+    // loggig the human and computer selection in the console
+    console.log(newCompSelection);
+    console.log(newHumanSelection);
+  }
 }
 
+playGame();
 
+// add function to check final scores
+function finalScore(finalHumanScore, finalComputerScore) {
+  if (humanScore > computerScore) {
+    console.log(
+      "No! No more games. Finish him off. Finish him now! Gollum, Gollum!"
+    );
+  } else {
+    console.log("Bagginses lost, now we eats it whole.");
+  }
+}
 
- 
-   playGame();
-  // logging updated scores in the console
-  console.log(humanScore, computerScore);
-
-// add console message for final score and winner
+finalScore(humanScore, computerScore);
+// logging updated scores in the console
+console.log(humanScore, computerScore);
