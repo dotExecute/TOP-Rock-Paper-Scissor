49a500837cf74bd31fa3a9a13c31dc524e6fbd42
Swapney Raul <raulswapney97@gmail.com>
2025-08-25 13:55:19 +0530

    -Add functionality to play a single round and update score accordingly. -Add console.logs in order of the game execution

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 // generate random num between 0-3
 function getComputerChoice(){
     let randNum = Math.floor(Math.random() * 3);
@@ -11,11 +14,51 @@ function getComputerChoice(){
         return "scissor";
     }
 }
-console.log(getComputerChoice());
 
 // get human choice and store it
 function getHumanChoice(){
     let humanChoice = prompt("What's your Choice?");
+
+    // returning the choice
     return humanChoice;
 }
-console.log(getHumanChoice());
+
+// logic for a single round, just a bunch of if else statements
+function playRound(human, computer){
+    if(computer === "rock" && human === "scissor"){
+        console.log("Computer Wins!");
+        computerScore++;
+    }else if (computer === "scissor" && human == "paper"){
+        console.log("Computer Wins!");
+        computerScore++;
+    }else if (computer === "paper" && human == "rock"){
+        console.log("Computer Wins!");
+        computerScore++;
+    }else if(human === "rock" && computer === "scissor"){
+        console.log("Human Wins!");
+        humanScore++;
+    }else if (human === "scissor" && computer == "paper"){
+        console.log("Human Wins!");
+        humanScore++;
+    }else if (human === "paper" && computer == "rock"){
+        console.log("Human Wins!");
+        humanScore++;
+    }else{
+        console.log("Undefined");
+    }
+}
+
+  //putting the functions in a variables in order to later pass them as
+  // arguments for the playRound function logic
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+
+//   loggig the human and computer selection in the console
+  console.log(computerSelection);
+  console.log(humanSelection);
+
+  // calling the playRound function and passing the variabels containing functions
+  playRound(humanSelection, computerSelection);
+
+  // logging updated scores in the console
+  console.log(humanScore, computerScore);
